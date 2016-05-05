a033d4f48c6ccbfcbe5d8eb5eb8191bcdc3f7439
njuzss <zss.ai.3@gmail.com>
2016-05-05 15:31:05 +0800

    add GMM clutering method

--- main.m
+++ main.m
@@ -1,30 +1,30 @@
 clear;
-
-%%  add the imagePath
 vPath='D:\Experiments\MultiView\';
 imPath='Projections\';
 pPath='Patches\';
 cluPath = 'Clusters\';
-K=60;
+
 imClass = char('armchair','sofa','endtable','teatable');
-
-threshold_l = 0.1;
-threshold_h = 0.2;
-view = 13;
+K=50;         %max k among models
+view = 5;
 scene = 64;
+threshold_l = 0.1;
+threshold_h = 0.15;
 
-% for i = 1:4  
-%     genViewSet(K,vPath,deblank(imClass(i,:)));
-% end
-
-for j = 1:1
-    for i = 1:5
-%        run_randpatch( vPath,num2str(i),deblank(imClass(j,:)),imPath,pPath );
-%        run_GMM( K,vPath,num2str(i),deblank(imClass(j,:)),pPath,cluPath );
-%        run_idf(scene,view,K,vPath,cluPath,deblank(imClass(i,:)),threshold_l,threshold_h);
-%        run_idf_backup(K,vPath,num2str(i),cluPath,deblank(imClass(j,:)),threshold_l,threshold_h);
-%        run_genLabels(vPath,num2str(i),cluPath,deblank(imClass(j,:)));
-        genModelSet(K,vPath,num2str(i),imClass);
-        genViewSet(K,vPath,deblank(imClass(i,:)));
+%% clustering
+for j = 1:length(imClass(:,1))
+    for i = 1:1      
+%        randpatch( vPath,num2str(i),deblank(imClass(j,:)),imPath,pPath );
+        GMM( K,vPath,num2str(i),deblank(imClass(j,:)),pPath,cluPath );
+%        filterClu(K,scene,vPath,num2str(i),cluPath,deblank(imClass(j,:)),threshold_l,threshold_h);
     end
 end
+%% generate inter-view trainning data 
+% for i = 1:length(imClass(:,1))  
+%        
+%        genViewSet(K,vPath,deblank(imClass(i,:)));
+% end
+%% generate intra-view trainning data 
+% for i = 1:view
+%        genModelSet(K,vPath,num2str(i),view ,imClass);
+% end
